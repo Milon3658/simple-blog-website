b19f2ca5e1bc2b96121cf042e7cf830868337272
milon3658 <jan.milon.56@gmail.com>
2025-08-28 02:46:22 +0600

    ui changed

--- src/components/Bookmarks/Bookmark.jsx
+++ src/components/Bookmarks/Bookmark.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Bookmark = ({ bookmark }) => {
     const { title } = bookmark;
     return (
-        <div className='bg-white p-2 mb-2 rounded shadow-2xl'>
+        <div className='bg-white p-2 mb-2 rounded shadow-2xl border border-purple-200'>
             <h1>{title}</h1>
         </div>
     );
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -6,12 +6,14 @@ const Bookmarks = ({ bookmarks, readingTime }) => {
 
 
     return (
-        <div className='lg:w-1/3 background-gray-100 p-4 rounded shadow lg:mt-6 lg:mb-6 md:mt-6 md:mb-6'>
-            <div className='rounded pt-1 pb-1 pl-3 border border-purple-500 mb-2'>
+        <div className='flex flex-col lg:mt-6 lg:mb-6 md:mt-6 md:mb-6 lg:w-1/3 md:w-1/4'>
+            <div className='rounded pt-1 pb-1 pl-3 border border-purple-500 mb-3 bg-purple-100'>
                 <h4 className='text-purple-500 bold'>Spent time on read: {readingTime} min</h4>
             </div>
-            <h4 className=' pb-2'>Bookmarks: {bookmarks.length}</h4>
-            {bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)}
+            <div className='bg-gray-50 p-4 rounded-xl shadow h-full'>
+                <h4 className=' pb-2 font-bold'>Bookmarks: {bookmarks.length}</h4>
+                {bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)}
+            </div>
         </div>
 
     );
